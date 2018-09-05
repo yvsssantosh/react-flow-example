ad770c7f05913b9431dfc76424eed8c12a3555d1
Yadavalli Santosh <yvss.santosh@gmail.com>
2018-09-05 18:54:03 +0530

    Code with validation including proptype validation

--- src/components/authors/authorList.js
+++ src/components/authors/authorList.js
@@ -1,39 +1,38 @@
-'use strict';
+"use strict";
 
 var React = require('react');
+var Router = require('react-router');
+var Link = Router.Link;
 
 var AuthorList = React.createClass({
-    propTypes: {
-        authors: React.PropTypes.array.isRequired
-    },
-    render: function () {
-        var createAuthorRow = function (author) {
-            return (
-                <tr key={author.id}>
-                    <td>
-                        <a href={"/#authors/" + author.id}>{author.id}</a>
-                    </td>
-                    <td>
-                        {author.firstName} {author.lastName}
-                    </td>
-                </tr>
-            );
-        };
+	propTypes: {
+		authors: React.PropTypes.array.isRequired
+	},
 
-        return (
-            <div>
-                <table className="table">
-                    <thread>
-                        <th>ID</th>
-                        <th>Name</th>
-                    </thread>
-                    <tbody>
-                        {this.props.authors.map(createAuthorRow, this)}
-                    </tbody>
-                </table>
-            </div>
-        );
-    }
+	render: function() {
+		var createAuthorRow = function(author) {
+			return (
+				<tr key={author.id}>
+					<td><Link to="manageAuthor" params={{id: author.id}}>{author.id}</Link></td>
+					<td>{author.firstName} {author.lastName}</td>
+				</tr>
+			);
+		};
+
+		return (
+			<div>
+				<table className="table">
+					<thead>
+						<th>ID</th>
+						<th>Name</th>
+					</thead>
+					<tbody>
+						{this.props.authors.map(createAuthorRow, this)}
+					</tbody>
+				</table>
+			</div>
+		);
+	}
 });
 
 module.exports = AuthorList;
